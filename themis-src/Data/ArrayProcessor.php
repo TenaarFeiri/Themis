ca62f5a555a025debdae3dbc4a5ec037cc95a460
TenaarFeiri <tenaarfeiri@gmail.com>
2025-06-15 19:22:20 +0200

    Update ArrayProcessor.php
    - Variable name changes for SystemDataStorage, modified constructor to receive SDS object.
    
    - Added validation for associative arrays (isAssociative).

--- themis-src/Data/ArrayProcessor.php
+++ themis-src/Data/ArrayProcessor.php
@@ -1,33 +1,49 @@
 <?php
     namespace Themis\Data;
 
+    use Themis\System\SystemDataStorage;
     use InvalidArgumentException;
     class ArrayProcessor
     {
         // -- Properties --
-        private bool $inDebugMode = false;
+        private SystemDataStorage $systemData;
         // -- End Properties --
 
         // -- Constants --
         private const ERROR_WHITELIST_MISMATCH = "Error: Key '%s' is not in the whitelist.";
         private const ERROR_WHITELIST_MISMATCH_COUNT = "Error: Key count mismatch between options and whitelist.";
         // -- End Constants --
-        public function __construct(bool $inDebugMode)
+        public function __construct(SystemDataStorage $sysData)
         {
-            $this->inDebugMode = $inDebugMode;
-            if ($inDebugMode) {
+            $this->systemData = $sysData;
+            if ($this->systemData->inDebugMode) {
                 echo "ArrayProcessor initialized.", PHP_EOL;
             }
         }
 
-        public function generateWildcards(array $array, string $wildcard = "?") : string
+        public function generateWildcards(array $array, string $wildcard = "?") : array
         {
             $wildcardArray = [];
             $count = count($array);
             if ($count === 0) {
                 return [];
             }
-            return implode(",", array_fill(0, $count, $wildcard));
+            return array_fill(0, $count, $wildcard);
+        }
+
+        public function isAssociative(array $arr): bool 
+        {
+            return array_keys($arr) !== range(0, count($arr) - 1);
+        }
+
+        public function isInArray(array $array, $value) : bool
+        {
+            $index = array_search($value, $array, true);
+            if ($index === false) {
+                return false;
+            }
+            return true;
         }
         
     }
+
