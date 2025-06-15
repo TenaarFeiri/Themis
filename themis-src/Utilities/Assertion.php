0942e5442cd7ddcc48e72beadcffe7aa948d4f74
TenaarFeiri <tenaarfeiri@gmail.com>
2025-06-15 19:14:45 +0200

    Update Assertion.php
    - Added generic var assertion
    - Added assertion for maximum string length.

--- themis-src/Utilities/Assertion.php
+++ themis-src/Utilities/Assertion.php
@@ -2,7 +2,7 @@
     namespace Themis\Utilities;
 
     use InvalidArgumentException;
-    use Exception;
+    //use Exception;
 
     class Assertion
     {
@@ -16,7 +16,7 @@
         public static function noArraysAreEmpty(array $arrayContainer, string $message = "One or more arrays are empty."): void
         {
             foreach ($arrayContainer as $array) {
-                if (§is_array($array) || count($array) === 0) {
+                if (!is_array($array) || count($array) === 0) {
                     throw new InvalidArgumentException($message);
                 }
             }
@@ -29,11 +29,31 @@
             }
         }
 
-        public static function stringNotEmpty(string $string, string $message = "String should not be empty."): void
+        public static function stringIsNotEmpty(string $string, string $message = "String should not be empty."): void
         {
             if (empty(trim($string))) {
                 throw new InvalidArgumentException($message);
             }
         }
-        
+       
+        // This is a generic variable assertion.
+        // Check for empty vars, like strings, arrays, etc., but allow falsy values otherwise.
+        public static function varIsNotEmpty($variable, string $message = "Generic empty variable assertion failure.", bool $canBeNull = true) : void
+        {
+            // Null can be a valid variable.
+            if (empty($variable) 
+                && $variable !== 0 
+                && $variable !== "0" 
+                && $variable !== false 
+                && !($canBeNull && $variable === null)) {
+                throw new InvalidArgumentException($message);
+            }
+        }
+
+        public static function stringDoesNotExceedMaxCharacters(string $str, int $max = 255) : void
+        {
+            if (strlen($str) > $max) {
+                throw new InvalidArgumentException("Input exceeds maximum length of " . $max . " characters.");
+            }
+        }
     }
