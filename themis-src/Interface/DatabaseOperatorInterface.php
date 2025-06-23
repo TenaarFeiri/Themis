8d18739fec8a95af4270ef17781d9ea0b5e1eb0e
TenaarFeiri <tenaarfeiri@gmail.com>
2025-06-24 01:08:37 +0200

    Update DatabaseOperatorInterface.php

--- themis-src/Interface/DatabaseOperatorInterface.php
+++ themis-src/Interface/DatabaseOperatorInterface.php
@@ -2,8 +2,9 @@
     namespace Themis\Interface;
 
     use PDO;
+    use Themis\System\SystemDataStorage;
     interface DatabaseOperatorInterface
     {
-        public function __construct(bool $inDebugMode);
+        public function __construct(SystemDataStorage $sysData);
         public function startOrGetPDO(?string $connectTo = null, array $options = [], bool $getPdo = false) : PDO | bool;
     }
