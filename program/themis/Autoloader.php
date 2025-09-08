3acfdee8ecaf153d24c92ed5aa60a83b5961f59a
TenaarFeiri <tenaarfeiri@gmail.com>
2025-09-08 18:27:57 +0200

    Update Autoloader.php
    Improved error handling, SEH requires ThemisErrorLog now as well, and all PHP files that use the autoloader will have access.

--- program/themis/Autoloader.php
+++ program/themis/Autoloader.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/ThemisErrorLog.php';
+require_once __DIR__ . '/StrictErrorHandler.php';
 
 /**
  * Registers an autoloader function for classes in the 'Themis' namespace.
